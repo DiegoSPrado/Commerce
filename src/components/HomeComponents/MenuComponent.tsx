e8fd347aa5a6f8286c064461f1d57215a647167e
DiegoSPrado <159810695+DiegoSPrado@users.noreply.github.com>
2025-01-26 18:29:45 -0300

    Home Header update

--- src/components/HomeComponents/MenuComponent.tsx
+++ src/components/HomeComponents/MenuComponent.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import {  useState } from "react";
 import "../../styles/homeheader.css"
 import { Menu, X } from "lucide-react";
 
 export function MenuComponent () {
     const [openMenu, setOpenMenu] = useState(false);
-
       const changeMenu = () => {
         setOpenMenu(!openMenu);
-      }
+    };
+
     
     return(
         <div className="menu-container">
             <button className="ham-button" onClick={changeMenu}>
-                {openMenu ? <X size={35}/> : <Menu size={35}/>}
+                {openMenu ? <X size={35} color="#FF653F"/> : <Menu size={35} color="#FF653F"/>}
             </button>
             { openMenu &&  (<nav className="menu">
                 <ul>
